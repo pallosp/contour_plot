c0944ae7f18e4817c2661ee4eda1cd361ff7f800
Péter Pallos <pallosp@gmail.com>
2025-01-16 14:26:15 +0100

    Migrate from ts-jest to the much faster @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   testEnvironment: 'node',
-  transform: {
-    '^.+.tsx?$': ['ts-jest', {}],
-  },
+  transform: {'^.+.tsx?$': '@swc/jest'},
 };
