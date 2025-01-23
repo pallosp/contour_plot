bb7863c50da5b0ab437c242c3d43642f0d4b3e7c
Péter Pallos <pallosp@gmail.com>
2025-01-23 17:17:00 +0100

    Measure overhead with a checkerboard pattern

--- benchmark/benchmark.ts
+++ benchmark/benchmark.ts
@@ -12,6 +12,10 @@ function mandelbrot(x: number, y: number): number {
   return i % 6;
 }
 
+function checkerboard(x: number, y: number): boolean {
+  return x === y || Math.clz32(x - y & y - x) + 1 < Math.clz32(x & -x);
+}
+
 const PIXEL_SIZE = 1 / 128;
 const SAMPLE_DISTANCE = 1 / 4;
 const viewport = {
@@ -23,10 +27,10 @@ const viewport = {
 
 const bench = new Bench({warmupIterations: 5});
 
-const tree = new Quadtree(mandelbrot);
-tree.compute(viewport, SAMPLE_DISTANCE, PIXEL_SIZE);
+const mandelbrotTree = new Quadtree(mandelbrot);
+mandelbrotTree.compute(viewport, SAMPLE_DISTANCE, PIXEL_SIZE);
 
-bench.add('eval ∀ pixels', () => {
+bench.add('Mandelbrot ∀ px', () => {
   const xMin = viewport.x + PIXEL_SIZE / 2;
   const xMax = viewport.x + viewport.width - PIXEL_SIZE / 2;
   const yMin = viewport.y + PIXEL_SIZE / 2;
@@ -37,11 +41,25 @@ bench.add('eval ∀ pixels', () => {
     }
   }
 });
-bench.add('build tree', () => {
-  tree.compute(viewport, SAMPLE_DISTANCE, PIXEL_SIZE);
+bench.add('Mandelbrot tree', () => {
+  mandelbrotTree.compute(viewport, SAMPLE_DISTANCE, PIXEL_SIZE);
 });
-bench.add('build runs', () => {
-  tree.runs();
+bench.add('Mandelbrot runs', () => {
+  mandelbrotTree.runs();
+});
+
+const checkerboardTree = new Quadtree(
+    (x, y) => x === y || Math.clz32(x - y & y - x) + 1 < Math.clz32(x & -x));
+
+bench.add('Checkers ∀ px', () => {
+  for (let y = 1; y < 128; y += 2) {
+    for (let x = 1; x < 128; x += 2) {
+      checkerboard(x, y);
+    }
+  }
+});
+bench.add('Checkers tree', () => {
+  checkerboardTree.compute({x: 0, y: 0, width: 128, height: 128}, 16, 2);
 });
 
 await bench.run();
