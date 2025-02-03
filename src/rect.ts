88fe2c484215ac0543116465a357987fe3cec244
Péter Pallos <pallosp@gmail.com>
2025-02-03 09:46:11 +0100

    Add overlappingArea(rect1, rect2)

--- src/rect.ts
+++ src/rect.ts
@@ -22,3 +22,13 @@ export function alignToGrid(rect: Rect, gridSpacing: number): Rect {
     height: Math.ceil((rect.y + rect.height) / gridSpacing) * gridSpacing - y
   };
 }
+
+/** Calculates the overlapping area of two rectangles. */
+export function overlappingArea(rect1: Rect, rect2: Rect): number {
+  const width = Math.min(rect1.x + rect1.width, rect2.x + rect2.width) -
+      Math.max(rect1.x, rect2.x);
+  const height = Math.min(rect1.y + rect1.height, rect2.y + rect2.height) -
+      Math.max(rect1.y, rect2.y);
+
+  return width > 0 && height > 0 ? width * height : 0;
+}
--- tests/rect.test.ts
+++ tests/rect.test.ts
@@ -1,8 +1,27 @@
-import {alignToGrid} from '../src/rect';
+import {alignToGrid, overlappingArea} from '../src/rect';
 
 test('alignToGrid', () => {
   expect(alignToGrid({x: 3, y: 6, width: 9, height: 12}, 3))
       .toEqual({x: 3, y: 6, width: 9, height: 12});
   expect(alignToGrid({x: 1, y: 2, width: 3, height: 4}, 5))
       .toEqual({x: 0, y: 0, width: 5, height: 10});
+});
+
+test('overlappingArea', () => {
+  const rect = {x: 0, y: 0, width: 2, height: 2};
+
+  expect(overlappingArea(rect, {x: 1, y: 1, width: 1, height: 0})).toBe(0);
+  expect(overlappingArea(rect, {x: 1, y: 1, width: 0, height: 1})).toBe(0);
+
+  expect(overlappingArea(rect, {x: 0, y: 0, width: 1, height: 1})).toBe(1);
+  expect(overlappingArea(rect, {x: -1, y: 0, width: 1, height: 1})).toBe(0);
+  expect(overlappingArea(rect, {x: 0, y: -1, width: 1, height: 1})).toBe(0);
+  expect(overlappingArea(rect, {x: -1, y: -1, width: 2, height: 2})).toBe(1);
+
+  expect(overlappingArea(rect, {x: 1, y: 1, width: 1, height: 1})).toBe(1);
+  expect(overlappingArea(rect, {x: 2, y: 1, width: 1, height: 1})).toBe(0);
+  expect(overlappingArea(rect, {x: 1, y: 2, width: 1, height: 1})).toBe(0);
+  expect(overlappingArea(rect, {x: 1, y: 1, width: 2, height: 2})).toBe(1);
+
+  expect(overlappingArea(rect, {x: -1, y: -1, width: 4, height: 4})).toBe(4);
 });
