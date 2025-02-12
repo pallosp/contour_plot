6aa78ed153c1738028991ce70848ad20adb5d4f6
Péter Pallos <pallosp@gmail.com>
2025-02-12 11:18:11 +0100

    Benchmark Maps.groupBy

--- benchmark/data_structures.ts
+++ benchmark/data_structures.ts
@@ -3,8 +3,10 @@
 
 import {Bench} from 'tinybench';
 
-const bench =
-    new Bench({name: 'Adding 50k elements to key-value data structures'});
+const bench = new Bench({
+  name: 'Adding 50k elements to key-value data structures',
+  time: 500,
+});
 
 bench.add('float keyed Map', () => {
   const m = new Map<number, number>;
@@ -20,6 +22,7 @@ bench.add('10 int keyed Maps', () => {
     for (let j = 0; j < 10000; j += 2) m.set(j, j);
   }
 });
+
 bench.add('sparse Array, 0.5', () => {
   const a: number[] = new Array(100000);
   for (let i = 0; i < 100000; i += 2) a[i] = i;
@@ -31,6 +34,24 @@ bench.add('sparse Array, 0.1', () => {
 bench.add('sparse Array, 0.05', () => {
   const a: number[] = new Array(1000000);
   for (let i = 0; i < 1000000; i += 20) a[i] = i;
+});
+
+const sequence: number[] = [];
+for (let i = 0; i < 50000; i++) sequence.push(i);
+
+bench.add('Map.groupBy', () => {
+  Map.groupBy(sequence, el => el % 2);
+});
+bench.add('manual groupBy', () => {
+  const map = new Map<number, number[]>();
+  for (const el of sequence) {
+    const group = map.get(el % 2);
+    if (group) {
+      group.push(el);
+    } else {
+      map.set(el % 2, [el]);
+    }
+  }
 });
 
 await bench.run();
