9e7d9273ad0c20f86f31a6881ff951cd9fd96d51
Péter Pallos <pallosp@gmail.com>
2025-07-28 14:44:58 +0200

    Publish the AffineTransform interface

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {type ComputeStats, Plot} from './plot';
 export type {Rect} from './rect';
 export {runsToSvg, squaresToSvg, squaresToText} from './render';
-export type {Run, Square} from './types';
+export type {AffineTransform, Run, Square} from './types';
